577d2f28406f1e6a7f66ee3c65f6397bf4d41e02
gogwangsin <gwangsin1029@naver.com>
2023-09-25 21:12:43 +0900

    가만히 있는 소닉 그리기, 크기 키우기

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,10 +6,13 @@ from pico2d import *
 TUK_WIDTH, TUK_HEIGHT = 1080, 768
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
+sonic = load_image('sonic.png')
+# 850 x 1504
+# 대충 35 x 45
+
 
 def handle_events():
-
-    global gRunning
+    global gRunning, x_dir
 
     events = get_events()
     for event in events:
@@ -21,16 +24,23 @@ def handle_events():
 
 gRunning = True
 gFrame = 0
+x_dir = 0
+x = TUK_WIDTH // 2
+
+def not_move_sonic():
+    sonic.clip_draw(220, 1439, 35, 45, x, TUK_HEIGHT // 2, 90, 100)
+    pass
+
 
 while gRunning:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-
+    not_move_sonic()
     update_canvas()
     handle_events()
     if not gRunning:
         break
-    gFrame = ( gFrame + 1 ) % 8
+    gFrame = (gFrame + 1) % 8
     delay(0.05)
 
 close_canvas()
