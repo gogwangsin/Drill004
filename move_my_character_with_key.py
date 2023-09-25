c3e3eb12b754e4207aad417793e622537ffef100
gogwangsin <gwangsin1029@naver.com>
2023-09-25 23:25:08 +0900

    Debug overlap animation bug and make x,y tuple

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -17,6 +17,7 @@ x_dir = 0
 y_dir = 0
 x = TUK_WIDTH // 2
 y = TUK_HEIGHT // 2
+gBlocking = False
 
 def handle_events():
     global gRunning, x_dir, y_dir
@@ -51,36 +52,36 @@ def handle_events():
             elif event.key == SDLK_DOWN:
                 y_dir += 1
 
-
-
-
 def not_move_sonic():
     sonic.clip_draw(220, 1439, 35, 45, x, y, 90, 100)
 
-def right_move_sonic():
-    global x
-    if x + gSpeed > TUK_WIDTH-1-35:
+def block_move_sonic():
+    global x, y
+    if x + gSpeed > TUK_WIDTH - 1 - 35:
         x -= gSpeed
+    if x - gSpeed < 0 + 35:
+        x += gSpeed
+    if y + gSpeed > TUK_HEIGHT - 1:
+        y -= gSpeed
+    if y - gSpeed < 0 + 45:
+        y += gSpeed
+
+def right_move_sonic():
+    block_move_sonic()
     sonic.clip_draw(300 + gFrame * 38, 1438, 35, 45, x, y, 90, 100)
 
 def left_move_sonic():
-    global x
-    if x - gSpeed < 0 + 35:
-        x += gSpeed
+    block_move_sonic()
     sonic.clip_composite_draw(300 + gFrame * 38, 1438, 35, 45, 0, 'h', x, y, 90, 100)
 
 def up_move_sonic():
-    global y
-    if y + gSpeed > TUK_HEIGHT-1:
-        y -= gSpeed
+    block_move_sonic()
     if x_dir >= 0 and y_dir > 0:
         sonic.clip_draw(300 + gFrame * 38, 1438, 35, 45, x, y, 90, 100)
     elif x_dir < 0:
         sonic.clip_composite_draw(300 + gFrame * 38, 1438, 35, 45, 0, 'h', x, y, 90, 100)
 def down_move_sonic():
-    global y
-    if y - gSpeed < 0+45:
-        y += gSpeed
+    block_move_sonic()
     if x_dir >= 0 and y_dir < 0:
         sonic.clip_draw(300 + gFrame * 38, 1438, 35, 45, x, y, 90, 100)
     elif x_dir < 0:
@@ -89,26 +90,24 @@ def down_move_sonic():
 def RenderingSonic():
     if x_dir == 0 and y_dir == 0:
         not_move_sonic()
-    if x_dir == 1:
-        right_move_sonic()
-    if x_dir == -1:
-        left_move_sonic()
     if y_dir == 1:
         up_move_sonic()
     if y_dir == -1:
         down_move_sonic()
+    if x_dir == 1:
+        right_move_sonic()
+    if x_dir == -1:
+        left_move_sonic()
+
 
 while gRunning:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-
     handle_events()
-
     RenderingSonic()
     update_canvas()
     gFrame = (gFrame + 1) % 9
-    x += x_dir * gSpeed
-    y += y_dir * gSpeed
+    x, y = x + x_dir * gSpeed, y + y_dir * gSpeed
     delay(0.05)
 
 close_canvas()
