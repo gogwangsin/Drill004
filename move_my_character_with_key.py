262475945cf111263c010cadd1b908d1f5b99f39
gogwangsin <gwangsin1029@naver.com>
2023-09-25 20:49:37 +0900

    ESC키 설정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,12 +3,21 @@
 from pico2d import *
 
 
-TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+TUK_WIDTH, TUK_HEIGHT = 1080, 768
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 
 def handle_events():
-    pass
+
+    global gRunning
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            gRunning = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            gRunning = False
+
 
 gRunning = True
 gFrame = 0
@@ -19,8 +28,9 @@ while gRunning:
 
     update_canvas()
     handle_events()
+    if not gRunning:
+        break
     gFrame = ( gFrame + 1 ) % 8
     delay(0.05)
-    gRunning = False
 
 close_canvas()
