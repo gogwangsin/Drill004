e6f01710afdbce899b50b1c7b0d4cf57aebb0913
gogwangsin <gwangsin1029@naver.com>
2023-09-25 20:06:38 +0900

    배경화면 출력

---
+++ move_my_character_with_key.py
@@ -0,0 +1,26 @@
+
+
+from pico2d import *
+
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+
+def handle_events():
+    pass
+
+gRunning = True
+gFrame = 0
+
+while gRunning:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+
+    update_canvas()
+    handle_events()
+    gFrame = ( gFrame + 1 ) % 8
+    delay(0.05)
+    gRunning = False
+
+close_canvas()
